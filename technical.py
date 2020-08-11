b9dd8c6a64d38716960d40d01b2a04704f50f41a
alan5123 <alanparker5123@gmail.com>
2020-08-11 01:56:23 -0400

    1 left

---
+++ technical.py
@@ -0,0 +1,17 @@
+# Print out all of the strings in the following array in alphabetical order, each on a separate line.
+x = ['Waltz', 'Tango', 'Viennese Waltz', 'Foxtrot', 'Cha Cha', 'Samba', 'Rumba', 'Paso Doble', 'Jive']
+# The expected output is:
+# 'Cha Cha'
+# 'Foxtrot'
+# 'Jive'
+# 'Paso Doble'
+# 'Rumba'
+# 'Samba'
+# 'Tango'
+# 'Viennese Waltz'
+# 'Waltz'
+# You may use whatever programming language you'd like.
+
+x.sort()
+for y in x:
+    print(y)
